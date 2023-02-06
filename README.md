47b3fded47ec2986eb0cd7d7647feb951f0be66f
R4chel <rae84@cornell.edu>
2023-02-06 14:22:58 -0500

    ignore page size parameter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# vsketch template
+# Rachel's vsketch template
 
 Template sketch for the [vsketch](https://github.com/abey79/vsketch) project management tools.
+
+This is based on defualts I'm currently using and hopefully will evolve over time.
--- {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
+++ {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
@@ -1,13 +1,18 @@
 import vsketch
+from shapely.geometry import Point
 
 
 class {{cookiecutter.class_name}}(vsketch.SketchClass):
     # Sketch parameters:
-    # radius = vsketch.Param(2.0)
+    debug = vsketch.Param(False)
+    width = vsketch.Param(5., decimals=2, unit="in")
+    height = vsketch.Param(3., decimals=2, unit="in")
+
+    def random_point(self, vsk: vsketch.Vsketch):
+        return Point(vsk.random(0, self.width), vsk.random(0, self.height))
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
-        vsk.size("{{cookiecutter.page_size}}", landscape={{cookiecutter.landscape}})
-        vsk.scale("{{cookiecutter.preferred_unit}}")
+        vsk.size(f"{self.height}x{self.width}", landscape={{cookiecutter.landscape}}, center=False)
 
         # implement your sketch here
         # vsk.circle(0, 0, self.radius, mode="radius")
