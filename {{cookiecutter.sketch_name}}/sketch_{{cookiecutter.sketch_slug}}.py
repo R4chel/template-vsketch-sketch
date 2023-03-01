27e0d65ba7cb60f113d1ccc7d9de7ec57ff7b675
R4chel <rae84@cornell.edu>
2023-03-01 13:56:32 -0500

    attempt encoding margins. It didn't work if center=true so disabled that feature

--- {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
+++ {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
@@ -6,8 +6,8 @@ class {{cookiecutter.class_name}}(vsketch.SketchClass):
     debug = vsketch.Param(False)
     width = vsketch.Param(5., decimals=2, unit="in")
     height = vsketch.Param(3., decimals=2, unit="in")
+    margin = vsketch.Param(0.1, decimals=3, unit="in")
     landscape = vsketch.Param({{cookiecutter.landscape}})
-    center = vsketch.Param(True)
     pen_width = vsketch.Param(0.7, decimals=3, min_value=1e-10, unit="mm")
     num_layers = vsketch.Param(1)
     # radius = vsketch.Param(1.0, decimals=3, unit="in")
@@ -16,7 +16,10 @@ class {{cookiecutter.class_name}}(vsketch.SketchClass):
         return Point(vsk.random(0, self.width), vsk.random(0, self.height))
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
-        vsk.size(f"{self.height}x{self.width}", landscape=self.landscape, center=self.center)
+        vsk.size(f"{self.height}x{self.width}", landscape=self.landscape, center=False)
+        self.width = self.width - 2 * self.margin
+        self.height = self.height - 2 * self.margin
+        vsk.translate(self.margin, self.margin)
         vsk.penWidth(f"{self.pen_width}")
 
         # implement your sketch here
